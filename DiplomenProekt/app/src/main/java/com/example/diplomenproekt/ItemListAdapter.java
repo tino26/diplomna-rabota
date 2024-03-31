3bff1db52acce7b335a3f06299cb4da2ca3ead15
Alex-Valentino Georgiev <aresfrend13@gmail.com>
2024-03-31 07:30:24 +0300

    Fix grid view for device cards

--- DiplomenProekt/app/src/main/java/com/example/diplomenproekt/ItemListAdapter.java
+++ DiplomenProekt/app/src/main/java/com/example/diplomenproekt/ItemListAdapter.java
@@ -1,5 +1,7 @@
 package com.example.diplomenproekt;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -8,11 +10,15 @@ import android.widget.TextView;
 import java.util.List;
 
 public class ItemListAdapter extends BaseAdapter {
+    Context context;
     private final List<String> titles;
+    LayoutInflater inflater;
 
-    public ItemListAdapter(List<String> titles) {
+    public ItemListAdapter(Context applicationContext, List<String> titles) {
         super();
+        this.context = context;
         this.titles = titles;
+        inflater = (LayoutInflater.from(applicationContext));
     }
     @Override
     public int getCount() {
@@ -30,9 +36,10 @@ public class ItemListAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        TextView textView = new TextView(convertView.getContext());
+    public View getView(int position, View card_view, ViewGroup parent) {
+        card_view = inflater.inflate(R.layout.item_card_view, null);
+        TextView textView = (TextView) card_view.findViewById(R.id.device_name);
         textView.setText(titles.get(position));
-        return textView;
+        return card_view;
     }
 }
--- DiplomenProekt/app/src/main/java/com/example/diplomenproekt/ui/home/HomeFragment.java
+++ DiplomenProekt/app/src/main/java/com/example/diplomenproekt/ui/home/HomeFragment.java
@@ -4,14 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.GridView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.diplomenproekt.ItemListAdapter;
 import com.example.diplomenproekt.R;
 import com.example.diplomenproekt.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
@@ -19,17 +23,21 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
-//        binding = FragmentHomeBinding.inflate(inflater, container, false);
-//        View root = binding.getRoot();
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-        View contentView = inflater.inflate(R.layout.fragment_home, container, false);
-        ListView listView = contentView.findViewById(R.id.items_container);
-        return contentView;
+//        View contentView = inflater.inflate(R.layout.fragment_home, container, false);
+        GridView listView = root.findViewById(R.id.items_container);
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<4;i++)
+            list.add("Item "+i);
 
-//        return root;
+        ItemListAdapter listAdapter = new ItemListAdapter(getContext(), list);
+        listView.setAdapter(listAdapter);
+
+        return root;
     }
 
     @Override
